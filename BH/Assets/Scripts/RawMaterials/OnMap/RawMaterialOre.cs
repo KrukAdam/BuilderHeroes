0e28647555c886be4e0a6a5917e802e50943fbad
Kruk Adam <adamkruk.ka@gmail.com>
2022-01-25 12:59:25 +0100

    Added raw materials (ore) prefabs
    Added raw materials sprites

---
+++ BH/Assets/Scripts/RawMaterials/OnMap/RawMaterialOre.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RawMaterialOre : RawMaterial
+{
+    protected override void OnInteraction()
+    {
+        base.OnInteraction();
+        //TODO chance to spawn golem on first hit
+    }
+}
